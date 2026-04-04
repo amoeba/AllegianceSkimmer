7f607c3ef8dbcaabbf4ebe81eb004d335eb97979
Bryce Mecum <petridish@gmail.com>
2026-04-04 11:01:27 -0700

    Create build and release workflows (#9)
    Also repacks the final DLL to be self-contained with ILPack. Not tested yet.

--- AllegianceSkimmer/scripts/post-build.ps1
+++ AllegianceSkimmer/scripts/post-build.ps1
@@ -1,8 +1,14 @@
-param([string]$NuGetPackageRoot, [string]$SolutionDir);
+param([string]$NuGetPackageRoot, [string]$ProjectDir);
+
+$NuGetPackageRoot = $NuGetPackageRoot.TrimEnd('\')
+$ProjectDir = $ProjectDir.TrimEnd('\')
+
+$makensis = Join-Path $NuGetPackageRoot "nsis-tool\3.0.8\tools\makensis.exe"
+$nsiFile = Join-Path $ProjectDir "scripts\installer.nsi"
 
 If ($Env:OS -ne "" -and $Env:OS -ne $null -and $Env:OS.ToLower().Contains("windows")) {
-    "$($NuGetPackageRoot)nsis-tool\3.0.8\tools\makensis.exe $($ProjectDir)scripts\installer.nsi" | Invoke-Expression
+    & $makensis $nsiFile
 }
 else {
-    "makensis $($ProjectDir)scripts\installer.nsi" | Invoke-Expression
+    & makensis $nsiFile
 }
